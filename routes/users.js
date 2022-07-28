1d6b762d714a617addf6f4eb2efe7d44acf22f6b
delvinsaji <91379593+delvinsaji@users.noreply.github.com>
2022-07-28 19:09:14 +0530

    aa

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
+var MongoClient = require("mongodb").MongoClient;
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var url = "mongodb://localhost:27017/mydb";
+
+router.post("/chat", function (req, res, next) {
+  from = req.body.from;
+  to = req.body.to;
+
+  query1 = { user1: from, user2: to };
+  query2 = { user1: to, user1: from };
+  MongoClient(url, function (err, data) {
+    var dbo = data.db("mydb");
+    dbo.collection("chats").query(query1 || query2, function (err, result) {
+      if (err) {
+        res.send("error");
+      }
+      res.send(result.messages);
+    });
+  });
 });
 
 module.exports = router;

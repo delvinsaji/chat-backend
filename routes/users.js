5a5dba439c73ffe9f652320c16789029ff987899
delvinsaji <91379593+delvinsaji@users.noreply.github.com>
2022-07-28 19:20:16 +0530

    chats

--- routes/users.js
+++ routes/users.js
@@ -10,13 +10,44 @@ router.post("/chat", function (req, res, next) {
 
   query1 = { user1: from, user2: to };
   query2 = { user1: to, user1: from };
-  MongoClient(url, function (err, data) {
+  MongoClient.connect(url, function (err, data) {
     var dbo = data.db("mydb");
     dbo.collection("chats").query(query1 || query2, function (err, result) {
       if (err) {
         res.send("error");
       }
       res.send(result.messages);
+    });
+  });
+});
+
+router.post("/insert", function (req, res, next) {
+  from = req.body.from;
+  to = req.body.to;
+
+  message = req.body.message;
+  query1 = { user1: from, user2: to };
+  query2 = { user1: to, user1: from };
+  MongoClient.connect(url, function (req, res, next) {
+    var dbo = data.db("mydb");
+    dbo.collection("chats").query(query1 || query2, function (err, result) {
+      if (err) {
+        res.send("Error");
+      }
+      if (!result) {
+        dbo
+          .collection("chats")
+          .create({ user1: from, user2: to, messages: [[messages, from]] });
+      } else {
+        mess = result.messages;
+        mess.push([message, from]);
+        dbo.collection("chats").updateOne(result, mess, function (err, data) {
+          if (err) {
+            res.send("error2");
+          }
+          res.send("message sent");
+        });
+      }
     });
   });
 });

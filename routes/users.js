e0f9d9ab3247e3e897eac258a0455d9662912024
delvinsaji <91379593+delvinsaji@users.noreply.github.com>
2022-07-28 20:45:24 +0530

    final

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const { query } = require("express");
 var express = require("express");
 var router = express.Router();
 var MongoClient = require("mongodb").MongoClient;
@@ -5,14 +6,17 @@ var MongoClient = require("mongodb").MongoClient;
 var url = "mongodb://localhost:27017/mydb";
 
 router.post("/chat", function (req, res, next) {
-  from = req.body.from;
-  to = req.body.to;
+  const from = req.body.from;
+  const to = req.body.to;
 
-  query1 = { user1: from, user2: to };
-  query2 = { user1: to, user1: from };
+  const query1 = { user1: from, user2: to };
+  const query2 = { user1: to, user1: from };
   MongoClient.connect(url, function (err, data) {
+    if (err) {
+      res.send("error1");
+    }
     var dbo = data.db("mydb");
-    dbo.collection("chats").query(query1 || query2, function (err, result) {
+    dbo.collection("chats").findOne(query1 || query2, function (err, result) {
       if (err) {
         res.send("error");
       }
@@ -22,31 +26,44 @@ router.post("/chat", function (req, res, next) {
 });
 
 router.post("/insert", function (req, res, next) {
-  from = req.body.from;
-  to = req.body.to;
+  const from = req.body.from;
+  const to = req.body.to;
 
-  message = req.body.message;
-  query1 = { user1: from, user2: to };
-  query2 = { user1: to, user1: from };
-  MongoClient.connect(url, function (req, res, next) {
+  const message = req.body.message;
+
+  const query1 = { user1: from, user2: to };
+  const query2 = { user1: to, user1: from };
+  MongoClient.connect(url, function (err, data) {
     var dbo = data.db("mydb");
-    dbo.collection("chats").query(query1 || query2, function (err, result) {
+    dbo.collection("chats").findOne(query1 || query2, function (err, result) {
       if (err) {
         res.send("Error");
       }
       if (!result) {
         dbo
           .collection("chats")
-          .create({ user1: from, user2: to, messages: [[messages, from]] });
+          .insertOne(
+            { user1: from, user2: to, messages: [[message, from]] },
+            function (err, result) {
+              if (err) {
+                res.send("error3");
+              }
+              res.send("message sent");
+            }
+          );
       } else {
-        mess = result.messages;
-        mess.push([message, from]);
-        dbo.collection("chats").updateOne(result, mess, function (err, data) {
-          if (err) {
-            res.send("error2");
-          }
-          res.send("message sent");
-        });
+        dbo
+          .collection("chats")
+          .updateOne(
+            result,
+            { $push: { messages: [message, from] } },
+            function (err, data) {
+              if (err) {
+                res.send("error2");
+              }
+              res.send("message sent");
+            }
+          );
       }
     });
   });
